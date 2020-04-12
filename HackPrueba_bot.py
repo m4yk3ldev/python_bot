e652e1acad891cf3ea9fd9f76a1a032d62a4204d
Maykel Arias Torres <dev0006@simelo.tech>
2020-04-12 00:01:53 -0400

    Exclude conf

---
+++ HackPrueba_bot.py
@@ -0,0 +1,13 @@
+from cfg import TOKEN
+
+from telegram.ext import Updater, CommandHandler
+
+
+def main():
+    print("Estpy vivo")
+    """Start the bot."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
