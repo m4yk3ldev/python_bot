e97343e8e36c2449295c004a5a39b6d68a12b10e
Maykel Arias Torres <dev0006@simelo.tech>
2020-04-13 21:13:17 -0400

    Agrgando cambios en user

--- db.py
+++ db.py
@@ -12,9 +12,21 @@ class User(Base):
     id = Column('id', Integer, primary_key=True)
     username = Column('username', String, unique=True)
     correo = Column('correo', String, unique=True)
-    telefono = Column('telefono', String, unique=True)
+    telefono = Column('telefono', String, unique=True, nullable=True)
     provincia = Column('prov', String)
     is_3d = Column('is_3d', Boolean)
+
+    def setCorreo(self, email):
+        self.correo = email
+
+    def setTelefono(self, telef):
+        self.telefono = telef
+
+    def setProvincia(self, prov):
+        self.provincia = prov
+
+    def setUsername(self, us):
+        self.username = us
 
 
 # Crear conexion
